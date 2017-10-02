5974cd8a02067af057d145a7ed9a3241e801f942
Romanov Dmitry <dimaromanov2010@gmail.com>
2017-10-02 18:55:31 +0300

    Added tim2.c and tim2.h

--- User/inc/beep.h
+++ User/inc/beep.h
@@ -2,4 +2,14 @@
 #define _BEEP_H
 #include "stm8s.h"
 
+//Buzer use tim2 timer
+
+
+typedef struct
+{
+  uint8_t long_flag;
+  uint8_t cycles;
+}Beep_StructTypeDef;
+
+
 #endif
---
+++ User/src/beep.c
@@ -0,0 +1,4 @@
+#include "beep.h"
+
+
+
